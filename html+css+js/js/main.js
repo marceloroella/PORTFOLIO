6a0d9808e0a2442c877a6f5f33e59cc4e31c4241
marceloroella <marceloroella@gmail.com>
2021-10-18 23:07:49 -0300

    permeira section finalizada

--- html+css+js/js/main.js
+++ html+css+js/js/main.js
@@ -1,5 +1,18 @@
+//DECLARANDO VARIAVEIS
 var btncontact = document.querySelector('.kj-btn-contact');
 
+//PAGE PRELOADER
+window.addEventListener('load', function () {
+    var pagePreloder = document.querySelector('.kj-preloader');
+    pagePreloder.classList.add('kj-fade-out');
+ 
+    setTimeout(function () {
+        pagePreloder.style.display = 'none';
+    }, 2000);
+});
+
+
+//ABRINDO E FECHANDO INFORMAÇÕES DE CONTATO
 btncontact.addEventListener('click', function(){
  var boxcontact = document.querySelector('.kj-contact-info');
  boxcontact.classList.toggle('kj-is-open');
